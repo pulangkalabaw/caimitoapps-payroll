f79c8658a55592e335d2b133b02c86e0e0008cb4
pollyreal1 <pollyreal73@gmail.com>
2019-03-26 17:12:40 +0800

    Added employee/user index and employee/user store

--- laravel55-template/app/Http/Kernel.php
+++ laravel55-template/app/Http/Kernel.php
@@ -19,6 +19,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
+        \Barryvdh\Cors\HandleCors::class,
     ];
 
     /**
@@ -35,6 +36,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Barryvdh\Cors\HandleCors::class,
         ],
 
         'api' => [
@@ -57,5 +59,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'cors' => \Barryvdh\Cors\HandleCors::class,
     ];
 }
--- laravel55-template/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel55-template/database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,9 +1,8 @@
-<?php
 
+<?php
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-
 class CreateUsersTable extends Migration
 {
     /**
@@ -15,14 +14,18 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('user_id')->unique();
+            $table->string('employee_code')->unique();
+            $table->string('lname');
+            $table->string('fname');
+            $table->string('mname')->nullable();
+            $table->string('suffix')->nullable();
             $table->string('email')->unique();
-            $table->string('password');
+            $table->string('password');          
             $table->rememberToken();
             $table->timestamps();
         });
     }
-
     /**
      * Reverse the migrations.
      *
