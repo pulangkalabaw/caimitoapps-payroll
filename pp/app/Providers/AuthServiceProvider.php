e154421dbb2093e0c1f5c4ce486cd521a8ee982f
jiromesarb <jiromesangelbaril@sfernando.sti.edu>
2019-04-08 11:10:46 +0800

    Created Master table for payroll

--- pp/app/Providers/AuthServiceProvider.php
+++ pp/app/Providers/AuthServiceProvider.php
@@ -31,9 +31,14 @@ class AuthServiceProvider extends ServiceProvider
         // the User instance via an API token or any other method necessary.
 
         $this->app['auth']->viaRequest('api', function ($request) {
-            if ($request->input('api_token')) {
-                return User::where('api_token', $request->input('api_token'))->first();
+            $header = $request->header('Api-Token');
+
+            if($header && $header == 'birds fly south'){
+                return new User();
             }
+            // if ($request->input('api_token')) {
+            //     return User::where('api_token', $request->input('api_token'))->first();
+            // }
         });
     }
 }
